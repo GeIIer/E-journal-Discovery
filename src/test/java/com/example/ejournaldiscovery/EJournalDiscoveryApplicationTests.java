67fa0f88b86574583af38ab4f7993762af70de92
ivanovad <ivanov@o-code.ru>
2024-12-19 16:28:51 +0400

    init

---
+++ src/test/java/com/example/ejournaldiscovery/EJournalDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejournaldiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EJournalDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
